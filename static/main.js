8d0d2817a6fb7673f291a0cef7385e3b50d1762b
Daniel Alkemic Czuba <alkemic7@gmail.com>
2016-06-26 18:14:37 +0200

    Cleanup

--- static/main.js
+++ static/main.js
@@ -1,53 +1,33 @@
 $(document).ready(function() {
-    var loading = $("#loading");
+    var loading = $('#loading');
 
-    $("#ceneo-form").submit(function(e) {
-        e.preventDefault();
-        var self = $(this);
-        var resultsUl = $('#ceneo-results');
-        resultsUl.empty().hide();
-        loading.show();
+    ['ceneo', 'allegro'].forEach(function(service) {
+        $('#' + service + '-form').submit(function(e) {
+            e.preventDefault();
+            var self = $(this);
+            var resultsUl = $('#' + service + '-results');
+            resultsUl.empty().hide();
+            loading.show();
 
-        $.post('/ceneo', self.serialize(), function(data){
-            loading.hide();
-            resultsUl.show();
-            console.log(data.result);
-            data.result.forEach(function(row) {
-                products = [];
-                row.products.forEach(function(product) {
-                    products.push(product.name + ' ' + product.price.toFixed(2));
+            $.post('/' + service, self.serialize(), function(data){
+                loading.hide();
+                resultsUl.show();
+                console.log(data.result);
+                data.result.forEach(function(row) {
+                    products = [];
+                    row.products.forEach(function(product) {
+                        products.push(
+                            product.name + ' ' + product.price.toFixed(2)
+                        );
+                    });
+                    $('<li><b>' + row.shop + ' ' + row.price.toFixed(2) + ' ' + row.products.length + ' prod.</b> (' + products.join(', ') + ')</li>').appendTo(resultsUl);
                 });
-                $('<li><b>' + row.shop + ' ' + row.price.toFixed(2) + ' ' + row.products.length + ' prod.</b> (' + products.join(', ') + ')</li>').appendTo(resultsUl);
+            }).fail(function(data){
+                loading.hide();
+                resultsUl.show();
+                $('<li class=\'error\'>Error occured!</li>').appendTo(resultsUl);
             });
-        }).fail(function(data){
-            loading.hide();
-            resultsUl.show();
-            $('<li class="error">Error occured!</li>').appendTo(resultsUl);
         });
-    });
 
-    $("#allegro-form").submit(function(e) {
-        e.preventDefault();
-        var self = $(this);
-        var resultsUl = $('#allegro-results');
-        resultsUl.empty().hide();
-        loading.show();
-
-        $.post('/allegro', self.serialize(), function(data){
-            loading.hide();
-            resultsUl.show();
-            console.log(data.result);
-            data.result.forEach(function(row) {
-                products = [];
-                row.products.forEach(function(product) {
-                    products.push(product.name + ' ' + product.price.toFixed(2));
-                });
-                $('<li><b>' + row.shop + ' ' + row.price.toFixed(2) + ' ' + row.products.length + ' prod.</b> (' + products.join(', ') + ')</li>').appendTo(resultsUl);
-            });
-        }).fail(function(data){
-            loading.hide();
-            resultsUl.show();
-            $('<li class="error">Error occured!</li>').appendTo(resultsUl);
-        });
     });
 });
